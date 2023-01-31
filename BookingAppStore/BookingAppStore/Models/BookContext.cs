5d1073aaf3ee18135eae53dc046936bf2868a506
ivanovaleksei <ivanov.aleksei020979@gmail.com>
2023-01-31 19:43:20 +0300

    Moduls2-5. Begin work. Query Processing. Models. Controller and View

---
+++ BookingAppStore/BookingAppStore/Models/BookContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace BookingAppStore.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set;}
+        public DbSet<Purchase> Purchases { get; set;}
+    }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "War and Peace", Author = "Lev Tolstoy", Price = 220 });
+            db.Books.Add(new Book { Name = "Fathers and Children", Author = "I. Turgenev", Price = 180 });
+            db.Books.Add(new Book { Name = "Seagull", Author = "A. Chehov", Price = 150 });
+
+            base.Seed(db);
+        }
+    }
+}
---
+++ HelloMvcAplication/HelloMvcAplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloMvcAplication.MvcApplication" Language="C#" %>
