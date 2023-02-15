2c7e5a23fd48bee93f49410ff8114b3b58d972e6
ivanovaleksei <ivanov.aleksei020979@gmail.com>
2023-02-15 17:39:34 +0300

    Modul 28. One to many

--- NavigatorApp/NavigatorApp/Controllers/HomeController.cs
+++ NavigatorApp/NavigatorApp/Controllers/HomeController.cs
@@ -17,7 +17,65 @@ namespace NavigatorApp.Controllers
             var players = db.Players.Include(p => p.Team);
             return View(players.ToList());
         }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            // Формируем список команд для передачи в представление
+            SelectList teams = new SelectList(db.Teams, "Id", "Name");
+            ViewBag.Teams = teams;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            // Добавляем игрока в таблицу
+            db.Players.Add(player);
+            db.SaveChanges();
+            // перенаправляем на главную страницу
+            return RedirectToAction("Index");
+        }
 
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            // Находим в БД футболиста
+            Player player = db.Players.Find(id);
+            if (player != null) 
+            {
+                // Создаем список команд для передачи в представление
+                SelectList teams = new SelectList(db.Teams, "Id", "Name", player.TeamId);
+                ViewBag.Teams = teams;
+                return View(player);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Edit(Player player)
+        {
+            db.Entry(player).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            // Находим в БД футболиста
+            Player player = db.Players.Find(id);
+            if (player != null)
+            {
+                db.Players.Remove(player);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
