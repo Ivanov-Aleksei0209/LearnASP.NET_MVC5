7eb482f7add133393711d1b913d38fe5104aad5e
ivanovaleksei <ivanov.aleksei020979@gmail.com>
2023-02-15 20:53:06 +0300

    Modul 30. Working with models many-to-many

--- ManyToManyApp/ManyToManyApp/Controllers/HomeController.cs
+++ ManyToManyApp/ManyToManyApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using ManyToManyApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,58 @@ namespace ManyToManyApp.Controllers
             return View(db.Students.ToList());
         }
 
+        public ActionResult Edit(int id = 0)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Courses = db.Courses.ToList();
+            return View(student);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student student, int[] selectedCourses)
+        {
+            Student newStudent = db.Students.Find(student.Id);
+            newStudent.Name = student.Name;
+            newStudent.Surname = student.Surname;
+
+            newStudent.Courses.Clear();
+            if (selectedCourses != null)
+            {
+                // получаем выбранные курсы
+                foreach (var c in db.Courses.Where(co => selectedCourses.Contains(co.Id)))
+                {
+                    newStudent.Courses.Add(c);
+                }
+            }
+
+            db.Entry(newStudent).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Courses = db.Courses.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student student, int[] selectedCourses)
+        {
+            if (selectedCourses != null)
+            {
+                // получаем выбранные курсы
+                foreach (var c in db.Courses.Where(co => selectedCourses.Contains(co.Id)))
+                {
+                    student.Courses.Add(c);
+                }
+            }
+            db.Students.Add(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
         public ActionResult Details(int id = 0)
         {
             Student student = db.Students.Find(id);
